aee42605a8701e5f1c1fa5159cb1cd5e14ac7932
Peter Berry <pwcberry@gmail.com>
2024-04-13 15:14:18 +1000

    Build database interface

--- src/sqlite.js
+++ src/sqlite.js
@@ -1,150 +1,95 @@
 /**
  * Module handles database management
- *
- * Server API calls the methods in here to query and update the SQLite database
  */
-
-// Utilities we need
-const fs = require("fs");
+import fs from "node:fs";
+import sqlite3Lib from "sqlite3";
+import {open} from "sqlite";
 
 // Initialize the database
-const dbFile = "./.data/choices.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const dbWrapper = require("sqlite");
-let db;
+const DB_FILE = "./.data/superheroes.db";
+const databaseExists = fs.existsSync(DB_FILE);
+const sqlite3 = sqlite3Lib.verbose();
 
-/* 
-We're using the sqlite wrapper so that we can make async / await connections
-- https://www.npmjs.com/package/sqlite
-*/
-dbWrapper
-  .open({
-    filename: dbFile,
+const db = await open({
+    filename: DB_FILE,
     driver: sqlite3.Database
-  })
-  .then(async dBase => {
-    db = dBase;
+});
 
-    // We use try and catch blocks throughout to handle any database errors
-    try {
-      // The async / await syntax lets us write the db operations in a way that won't block the app
-      if (!exists) {
-        // Database doesn't exist yet - create Choices and Log tables
+try {
+    if (!databaseExists) {
         await db.run(
-          "CREATE TABLE Choices (id INTEGER PRIMARY KEY AUTOINCREMENT, language TEXT, picks INTEGER)"
+            `
+          CREATE TABLE Superhero (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, image_file TEXT NOT NULL);
+          CREATE TABLE Response (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id TEXT NOT NULL, superhero_id INTEGER NOT NULL,
+            CONSTRAINT superhero_fk FOREIGN KEY (superhero_id) REFERENCES Superhero (id));   
+          CREATE VIEW Results AS SELECT name, image_file, COUNT(r.id) AS votes 
+            FROM Superhero s LEFT JOIN Response r ON s.id = superhero_id GROUP BY name, image_file;`
         );
 
-        // Add default choices to table
         await db.run(
-          "INSERT INTO Choices (language, picks) VALUES ('HTML', 0), ('JavaScript', 0), ('CSS', 0)"
+            `
+              INSERT INTO Superhero (name, image_file) VALUES
+                ('Batman', 'batman.jpg'),
+                ('Black Panther', 'blackpanther.jpg'),
+                ('Black Widow', 'blackwidow.jpg'),
+                ('Bucky Barnes', 'buckybarnes.jpg'),
+                ('Captain America', 'captainamerica.jpg'),
+                ('Captain Marvel', 'captainmarvel.jpg'),
+                ('Deadpool', 'deadpool.jpg'),
+                ('Doctor Strange', 'doctorstrange.jpg'),
+                ('Groot', 'groot.jpg'),
+                ('Hawkeye', 'hawkeye.jpg'),
+                ('Hulk', 'hulk.jpg'),
+                ('Iron Man', 'ironman.jpg'),
+                ('Peter Quill', 'starlord.jpg'),
+                ('Scarlet Witch', 'scarletwitch.jpg'),
+                ('Shang Chi', 'shangchi.jpg'),
+                ('She Hulk', 'shehulk.jpg'),
+                ('Spiderman', 'spiderman.jpg'),
+                ('Storm', 'storm.jpg'),
+                ('Superman', 'superman.jpg'),
+                ('The Doctor', 'thedoctor.jpg'),
+                ('Thor', 'thor.jpg'),
+                ('Wasp', 'wasp.jpg'),
+                ('Wolverine', 'wolverine.jpg'),
+                ('Wonder Woman', 'wonderwoman.jpg');
+              `
         );
-
-        // Log can start empty - we'll insert a new record whenever the user chooses a poll option
-        await db.run(
-          "CREATE TABLE Log (id INTEGER PRIMARY KEY AUTOINCREMENT, choice TEXT, time STRING)"
-        );
-      } else {
+    } else {
         // We have a database already - write Choices records to log for info
-        console.log(await db.all("SELECT * from Choices"));
-
-        //If you need to remove a table from the database use this syntax
-        //db.run("DROP TABLE Logs"); //will fail if the table doesn't exist
-      }
-    } catch (dbError) {
-      console.error(dbError);
+        console.log(await db.all("SELECT * from Superhero"));
     }
-  });
+} catch (error) {
+    console.error(error);
+}
 
-// Our server script will call these methods to connect to the db
-module.exports = {
-  
-  /**
-   * Get the options in the database
-   *
-   * Return everything in the Choices table
-   * Throw an error in case of db connection issues
-   */
-  getOptions: async () => {
-    // We use a try catch block in case of db errors
+async function getSuperheroes() {
     try {
-      return await db.all("SELECT * from Choices");
-    } catch (dbError) {
-      // Database connection error
-      console.error(dbError);
+        return await db.all("SELECT * from Superhero");
+    } catch (error) {
+        // Database connection error
+        console.error(error);
     }
-  },
+}
 
-  /**
-   * Process a user vote
-   *
-   * Receive the user vote string from server
-   * Add a log entry
-   * Find and update the chosen option
-   * Return the updated list of votes
-   */
-  processVote: async vote => {
-    // Insert new Log table entry indicating the user choice and timestamp
+async function loadVote(userId, superheroId) {
     try {
-      // Check the vote is valid
-      const option = await db.all(
-        "SELECT * from Choices WHERE language = ?",
-        vote
-      );
-      if (option.length > 0) {
-        // Build the user data from the front-end and the current time into the sql query
-        await db.run("INSERT INTO Log (choice, time) VALUES (?, ?)", [
-          vote,
-          new Date().toISOString()
-        ]);
-
-        // Update the number of times the choice has been picked by adding one to it
-        await db.run(
-          "UPDATE Choices SET picks = picks + 1 WHERE language = ?",
-          vote
-        );
-      }
-
-      // Return the choices so far - page will build these into a chart
-      return await db.all("SELECT * from Choices");
-    } catch (dbError) {
-      console.error(dbError);
+        await db.run(`INSERT INTO Response (user_id, superhero_id) VALUES ('${userId}', '${superheroId}');`);
+    } catch (err) {
+        console.error(err);
     }
-  },
+}
 
-  /**
-   * Get logs
-   *
-   * Return choice and time fields from all records in the Log table
-   */
-  getLogs: async () => {
-    // Return most recent 20
+async function getResults() {
     try {
-      // Return the array of log entries to admin page
-      return await db.all("SELECT * from Log ORDER BY time DESC LIMIT 20");
-    } catch (dbError) {
-      console.error(dbError);
+        return db.all("SELECT * FROM Results ORDER BY votes DESC;");
+    } catch (error) {
+        console.error(error);
     }
-  },
+}
 
-  /**
-   * Clear logs and reset votes
-   *
-   * Destroy everything in Log table
-   * Reset votes in Choices table to zero
-   */
-  clearHistory: async () => {
-    try {
-      // Delete the logs
-      await db.run("DELETE from Log");
-
-      // Reset the vote numbers
-      await db.run("UPDATE Choices SET picks = 0");
-
-      // Return empty array
-      return [];
-    } catch (dbError) {
-      console.error(dbError);
-    }
-  }
+export {
+    getSuperheroes,
+    getResults,
+    loadVote
 };
